b78818b08fb4013cc46e8de52c209c2081f610d1
Yoann Moreau <yoann.moreau@tutanota.com>
2022-10-21 15:36:07 +0200

    Add index page

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class IndexController extends AbstractController {
+
+	#[Route('/', name: 'app_index')]
+	public function index(): Response {
+		return $this->render('index.html.twig',
+				[
+						'message' => 'Welcome to your new controller!',
+						'path'    => 'src/Controller/IndexController.php',
+				]
+		);
+	}
+
+}
