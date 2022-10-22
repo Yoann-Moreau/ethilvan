bae20ba0a7140155bf21235fbf99a340a8f32871
Yoann Moreau <yoann.moreau@tutanota.com>
2022-10-22 14:13:23 +0200

    Display index texts on index page

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use App\Repository\TextRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,12 +13,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController {
 
 	#[Route('/', name: 'app_index')]
-	public function index(): Response {
+	public function index(TextRepository $text_repository): Response {
+
+		$index_texts = $text_repository->findBy(['page' => 'index'], ['text_order' => 'ASC']);
+
 		return $this->render('index.html.twig',
-				[
-						'message' => 'Welcome to your new controller!',
-						'path'    => 'src/Controller/IndexController.php',
-				]
+		                     [
+				                     'texts' => $index_texts,
+		                     ]
 		);
 	}
 
