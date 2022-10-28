e3b0b37245f49f5c012eb296025cad9d85f95dfc
Yoann Moreau <yoann.moreau@tutanota.com>
2022-10-28 12:31:13 +0200

    Add CSRF to login & add logout

--- src/Controller/IdentificationController.php
+++ src/Controller/IdentificationController.php
@@ -75,4 +75,11 @@ class IdentificationController extends AbstractController {
 		]);
 	}
 
+
+	#[Route('/logout', name: 'app_logout', methods: ['GET'])]
+	public function logout() {
+		// controller can be blank: it will never be called!
+		throw new \Exception('Don\'t forget to activate logout in security.yaml');
+	}
+
 }
