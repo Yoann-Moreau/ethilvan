2030183226a9ef757fdb8832dc7e211ca1f8d3c1
Yoann Moreau <yoann.moreau@tutanota.com>
2022-11-07 12:58:02 +0100

    Add controls in JS Autocomplete class

--- public/assets/js/classes/Autocomplete.js
+++ public/assets/js/classes/Autocomplete.js
@@ -7,6 +7,8 @@ export default class Autocomplete {
 		this.element = element;
 		this.parentElement = element.parentElement;
 
+		this.hoveredChoice = -1;
+
 		this.init();
 	}
 
@@ -16,7 +18,8 @@ export default class Autocomplete {
 	 */
 	init() {
 		this.createInputHtml();
-		this.updateChoicesOnRequest();
+		this.addUpdateChoicesListener();
+		this.addControlsListener();
 	}
 
 
@@ -71,13 +74,100 @@ export default class Autocomplete {
 	/**
 	 * Add event listener for choicesReceived
 	 */
-	updateChoicesOnRequest() {
+	addUpdateChoicesListener() {
 		this.element.addEventListener('choicesReceived', (e) => {
 			this.clearSuggestions();
 			if (this.element.value !== '') {
-				this.addSuggestions(e.detail.choices)
+				this.addSuggestions(e.detail.choices);
 			}
-		})
+		});
+	}
+
+
+	/**
+	 * Increment hoveredChoice if possible
+	 */
+	incrementHoveredChoice() {
+		const choices = this.choicesContainer.getElementsByClassName('choice');
+
+		if (this.hoveredChoice + 1 < choices.length) {
+			this.hoveredChoice++;
+		}
+		else if (choices.length > 0) {
+			this.hoveredChoice = 0;
+		}
+	}
+
+
+	/**
+	 * Decrement hoveredChoice if possible
+	 */
+	decrementHoveredChoice() {
+		const choices = this.choicesContainer.getElementsByClassName('choice');
+
+		if (this.hoveredChoice - 1 >= 0) {
+			this.hoveredChoice--;
+		}
+		else if (choices.length > 0) {
+			this.hoveredChoice = choices.length - 1;
+		}
+	}
+
+
+	/**
+	 * Add the class 'hovered' to the currently hovered choice
+	 */
+	displayHoveredChoice() {
+		const choices = this.choicesContainer.getElementsByClassName('choice');
+
+		for (const choice of choices) {
+			choice.classList.remove('hovered');
+		}
+		if (this.hoveredChoice > -1) {
+			choices[this.hoveredChoice].classList.add('hovered');
+		}
+	}
+
+
+	/**
+	 * Update the input value accrding to the selected choice
+	 */
+	updateInputValue() {
+		const choices = this.choicesContainer.getElementsByClassName('choice');
+
+		if (this.hoveredChoice > -1) {
+			this.element.value = choices[this.hoveredChoice].dataset.name;
+			this.element.dispatchEvent(new Event('input'));
+		}
+	}
+
+
+	/**
+	 * Add controls
+	 */
+	addControlsListener() {
+		this.element.addEventListener('keydown', (e) => {
+			if (e.key === 'ArrowDown') {
+				e.preventDefault();
+				this.incrementHoveredChoice();
+				this.displayHoveredChoice();
+			}
+			else if (e.key === 'ArrowUp') {
+				e.preventDefault();
+				this.decrementHoveredChoice();
+				this.displayHoveredChoice();
+			}
+			else if (e.key === 'Enter') {
+				e.preventDefault();
+				this.updateInputValue();
+				this.hoveredChoice = -1;
+				this.displayHoveredChoice();
+			}
+			else if (e.location === 0 || e.location === 3) { // letter, space or number
+				this.hoveredChoice = -1;
+				this.displayHoveredChoice();
+			}
+		});
 	}
 
 }
