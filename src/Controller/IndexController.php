8694831da8ab61b1a69df72951768bc7b7e12fa9
Yoann Moreau <yoann.moreau@tutanota.com>
2022-11-21 12:20:11 +0100

    Style index sections 'games' and 'us'

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use App\Repository\GameRepository;
 use App\Repository\TextRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,12 +14,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController {
 
 	#[Route('/', name: 'app_index')]
-	public function index(TextRepository $text_repository): Response {
+	public function index(TextRepository $text_repository, GameRepository $game_repository): Response {
 
 		$index_texts = $text_repository->findBy(['page' => 'index'], ['text_order' => 'ASC']);
+		$current_games = $game_repository->findBy(['current' => true], ['name' => 'ASC'], 3);
 
 		return $this->render('index.html.twig', [
-				'texts' => $index_texts,
+				'texts'         => $index_texts,
+				'current_games' => $current_games,
 		]);
 	}
 
