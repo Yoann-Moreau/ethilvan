d3f333dd1800ccfdc66366e4fb7efdce0fefbe7d
Yoann Moreau <yoann.moreau@tutanota.com>
2022-12-16 22:26:03 +0100

    Basic style for games page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class IndexController extends AbstractController {
+class HomeController extends AbstractController {
 
 	#[Route('/', name: 'app_index')]
 	public function index(TextRepository $text_repository, GameRepository $game_repository): Response {
@@ -19,9 +19,24 @@ class IndexController extends AbstractController {
 		$index_texts = $text_repository->findBy(['page' => 'index'], ['text_order' => 'ASC']);
 		$current_games = $game_repository->findBy(['current' => true], ['name' => 'ASC'], 3);
 
-		return $this->render('index.html.twig', [
+		return $this->render('front/index.html.twig', [
 				'texts'         => $index_texts,
 				'current_games' => $current_games,
+		]);
+	}
+
+
+	#[Route('/games', name: 'app_games')]
+	public function games(TextRepository $text_repository, GameRepository $game_repository): Response {
+
+		$games_texts = $text_repository->findBy(['page' => 'games'], ['text_order' => 'ASC']);
+		$current_games = $game_repository->findBy(['current' => true], ['name' => 'ASC'], 3);
+		$games = $game_repository->findBy([], ['name' => 'ASC']);
+
+		return $this->render('front/games.html.twig', [
+				'texts'         => $games_texts,
+				'current_games' => $current_games,
+				'games'         => $games,
 		]);
 	}
 
