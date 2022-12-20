b3566cf9fe923aa00b236c296d405447c7b3ab40
Yoann Moreau <yoann.moreau@tutanota.com>
2022-12-20 08:38:17 +0100

    Style periods CRUD

--- src/Form/PeriodType.php
+++ src/Form/PeriodType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Period;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,11 +15,37 @@ class PeriodType extends AbstractType {
 
 	public function buildForm(FormBuilderInterface $builder, array $options): void {
 		$builder
-				->add('type')
-				->add('name')
-				->add('year')
-				->add('start_date')
-				->add('end_date');
+				->add('type', ChoiceType::class, [
+						'label'   => "Type de période",
+						'choices' => [
+								'Année'     => 'year',
+								'Evènement' => 'event',
+						],
+				])
+				->add('name', TextType::class, [
+						'label' => 'Nom',
+				])
+				->add('year', IntegerType::class, [
+						'label' => 'Année',
+						'attr'  => [
+								'min' => 2023,
+								'max' => 2050,
+						],
+				])
+				->add('start_date', DateType::class, [
+						'label'  => 'Date de début',
+						'widget' => 'single_text',
+						'attr'   => [
+								'min' => '2023-01-01',
+						],
+				])
+				->add('end_date', DateType::class, [
+						'label'  => 'Date de fin',
+						'widget' => 'single_text',
+						'attr'   => [
+								'min' => '2023-01-01',
+						],
+				]);
 	}
 
 
