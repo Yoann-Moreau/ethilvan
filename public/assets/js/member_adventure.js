6d7b32af069197a84c9ef197521cf98fee9624f7
Yoann Moreau <yoann.moreau@tutanota.com>
2025-05-04 17:56:50 +0200

    Add Phaedra map

--- public/assets/js/member_adventure.js
+++ public/assets/js/member_adventure.js
@@ -2,19 +2,40 @@
 // Variables
 // ============================================================================
 
-const openTabletButton = document.querySelector(".icon.tablet");
+const openMapButton = document.querySelector('.icon.map-icon');
+const mapMenu = document.querySelector('.map-menu');
+const closeMapButton = document.querySelector('.map .close-button');
+const mapImage = document.querySelector('.map');
+
+const openTabletButton = document.querySelector(".icon.tablet-icon");
 const tabletMenu = document.querySelector('.tablet-menu');
 const tabletContainer = document.querySelector('.tablet-container');
 const closeTabletButton = document.querySelector('.tablet .button-container');
 
 const textTitles = document.querySelectorAll('.text-title');
 
+const menus = document.querySelector('.menus');
+
 
 // ============================================================================
 // Functions
 // ============================================================================
 
+function displayMap() {
+	hideTablet();
+	hideMenus();
+	mapImage.classList.remove('hidden');
+}
+
+
+function hideMap() {
+	displayMenus();
+	mapImage.classList.add('hidden');
+}
+
+
 function displayTablet() {
+	hideMap();
 	tabletContainer.classList.remove('hidden');
 }
 
@@ -32,6 +53,16 @@ function toggleText(title) {
 }
 
 
+function hideMenus() {
+	menus.classList.add('hidden');
+}
+
+
+function displayMenus() {
+	menus.classList.remove('hidden');
+}
+
+
 // ============================================================================
 // Code to execute
 // ============================================================================
@@ -40,6 +71,10 @@ function toggleText(title) {
 // ============================================================================
 // Event listeners
 // ============================================================================
+
+openMapButton.addEventListener('click', displayMap);
+mapMenu.addEventListener('click', displayMap);
+closeMapButton.addEventListener('click', hideMap);
 
 openTabletButton.addEventListener('click', displayTablet);
 tabletMenu.addEventListener('click', displayTablet);
